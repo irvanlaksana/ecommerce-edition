ba236affc003afcf164c53741b8d97416727965b
irvanlaksana <88578657+irvanlaksana@users.noreply.github.com>
2026-06-11 13:32:27 +0700

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: './', // Tambahkan ini agar file HTML tidak tersesat di HP/Server
+  base: './', 
 })
